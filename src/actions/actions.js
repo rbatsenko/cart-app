ddc174d077ffac4ccb31bd8f4e06a8035daae558
rbatsenko <rbatsenko@gmail.com>
2018-10-29 08:36:42 +0100

    remove update action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -23,12 +23,3 @@ export const removeFromCart = ( { id } = {} ) => ({
   type: 'REMOVE_FROM_CART',
   id,
 });
-
-// updateCartItem
-export const updateCartItem = (id, count) => (
-  {
-    type: 'UPDATE_CART_ITEM',
-    id,
-    count,
-  }
-);
